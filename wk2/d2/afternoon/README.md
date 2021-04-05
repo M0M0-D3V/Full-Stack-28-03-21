620fabf65cfad4354ddfb3acdb9cb3b18fe91786
Monica Hong <momo.star@gmail.com>
2021-04-04 19:36:22 -0700

    pokemon

---
+++ wk2/d2/afternoon/README.md
@@ -0,0 +1,8 @@
+# USING APIs WITH VANILLA JAVASCRIPT
+
+## We know enough simple things to do more complex things!
+
+### TIPS:
+- read the code slowly
+- console.log() is your friend
+- google is also your friend
---
+++ wk2/d2/afternoon/more.js
@@ -0,0 +1,19 @@
+console.log("We made it!");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hey our document is ready!");
+  for (var i = 1; i <= 150; i++) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        // how do we retrieve just the images for the pokemon from the api?
+        console.log(data.sprites.front_default);
+        var body = document.querySelector("body");
+        //   console.log(body);
+        var pokemon = document.createElement("img");
+        var pokemonImgUrl = data.sprites.front_default;
+        pokemon.src = pokemonImgUrl;
+        body.appendChild(pokemon);
+      });
+  }
+});
