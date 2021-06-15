3248a57cd631fa9fe1a1dc34c9fdb8d263303651
Monica Hong <momo.star@gmail.com>
2021-06-14 22:06:25 -0700

    d3 algo

--- wk10/d2/algo.js
+++ wk10/d2/algo.js
@@ -83,13 +83,13 @@ function categoryTableBuilder(items) {
   Bonus: solve in O(n) time
 */
 
-// const S1 = "ab#c";
-// const T1 = "ad#c";
+// const S1 = "ab#c";  ac
+// const T1 = "ad#c";  ac
 // const expected1 = true;
 // // Explanation: Both S and T become "ac"
 
-// const S2 = "ab##";
-// const T2 = "c#d#";
+// const S2 = "ab##";  
+// const T2 = "c#d#";  
 // const expected2 = true;
 // // Explanation: Both S and T become ""
 
@@ -104,5 +104,61 @@ function categoryTableBuilder(items) {
 // // Explanation: S becomes "c" while T becomes "b".
 
 function backspaceStringCompare(S, T) {
-  // code here
+  const sBackspaced = getBackspacedStack(S);
+  const tBackspaced = getBackspacedStack(T);
+
+  if (sBackspaced.length !== tBackspaced.length) {
+    return false;
+  }
+
+  for (let i = 0; i < sBackspaced.length; i++) {
+    if (sBackspaced[i] !== tBackspaced[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getBackspacedStack(str) {
+  const backspacedStack = [];
+
+  for (const char of str) {
+    if (char !== "#") {
+      backspacedStack.push(char);
+    } else if (backspacedStack.length > 0) {
+      backspacedStack.pop();
+    }
+  }
+
+  return backspacedStack;
+}
+
+// another solution
+function backspaceCompare2(S, T) {
+  let sBackspaced = processBackspaces(S);
+  let tBackspaced = processBackspaces(T);
+
+  if (sBackspaced === tBackspaced) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function processBackspaces(s) {
+  let backspaceCount = 0;
+  let newS = "";
+
+  for (let i = s.length - 1; i >= 0; --i) {
+    let isBackspace = s[i] === "#";
+
+    if (backspaceCount > 0 && !isBackspace) {
+      backspaceCount--;
+    } else if (isBackspace) {
+      backspaceCount++;
+    } else {
+      newS = s[i] + newS;
+    }
+  }
+  return newS;
 }
