3c91df28fea1beb52a01b67c5cb947a16754b0ee
Monica Hong <momo.star@gmail.com>
2021-06-15 00:20:58 -0700

    d3 solutions

--- wk10/d3/algo.js
+++ wk10/d3/algo.js
@@ -21,6 +21,11 @@
 
 function addHonorific(honorific, fullNames) {
   // code here
+  return fullNames.map((fullName) =>
+    fullName
+      .split(", ")
+      .reduce((lastName, firstName) => `${honorific} ${firstName} ${lastName}`)
+  );
 }
 
 /*****************************************************************************/
@@ -38,31 +43,52 @@ function addHonorific(honorific, fullNames) {
   Bonus, solve without .split
 */
 
-// const test1V1 = "0.1";
-// const test1V2 = "1.1";
-// const expected1 = -1;
+const test1V1 = "0.1";
+const test1V2 = "1.1";
+const expected1 = -1;
 
-// const test2V1 = "1.0.1";
-// const test2V2 = "1";
-// const expected2 = 1;
+const test2V1 = "1.0.1";
+const test2V2 = "1";
+const expected2 = 1;
 
-// const test3V1 = "7.5.2.4";
-// const test3V2 = "7.5.3";
-// const expected3 = -1;
+const test3V1 = "7.5.2.4";
+const test3V2 = "7.5.3";
+const expected3 = -1;
 
-// const test4V1 = "7.5.2.4";
-// const test4V2 = "7.5.2";
-// const expected4 = 1;
+const test4V1 = "7.5.2.4";
+const test4V2 = "7.5.2";
+const expected4 = 1;
 
-// const test5V1 = "1.01";
-// const test5V2 = "1.001";
-// const expected5 = 0;
-// // Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
+const test5V1 = "1.01.";
+const test5V2 = "1.001.";
+const expected5 = 0;
+// Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
 
-// const test6V1 = "1.0.1";
-// const test6V2 = "1";
-// const expected6 = 1;
+const test6V1 = "1.0.1";
+const test6V2 = "1";
+const expected6 = 1;
 
 function compareVersionNumbers(v1, v2) {
   // code here
+  const v1Split = v1.split("."); // array contains: 7, 5, 2, 4
+  console.log(v1Split);
+  const v2Split = v2.split("."); // array contains: 7, 5, 2
+
+  for (let i = 0; i < v1Split.length || i < v2Split.length; i++) {
+    // || 0 means if whatever is to the left of || is falsy, use 0 as the value instead
+    const v1RevisionNum = parseInt(v1Split[i]) || 0;
+    console.log(v1RevisionNum);
+    const v2RevisionNum = parseInt(v2Split[i]) || 0;
+
+    if (v1RevisionNum > v2RevisionNum) {
+      return 1;
+    } else if (v1RevisionNum < v2RevisionNum) {
+      return -1;
+    }
+  }
+  return 0;
 }
+let result1 = compareVersionNumbers(test4V1, test4V2);
+let result2 = compareVersionNumbers(test5V1, test5V2);
+console.log(result1);
+console.log(result2);
