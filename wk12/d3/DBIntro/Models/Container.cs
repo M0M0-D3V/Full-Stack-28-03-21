a5ff7d48a9d335b879a2457b07038641c4c70e9a
Monica Hong <momo.star@gmail.com>
2021-06-29 06:29:14 -0700

    Container Method and Partials!

---
+++ wk12/d3/DBIntro/Models/Container.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace DBIntro.Models
+{
+    public class Container
+    {
+        public Movie Movie { get; set; }
+        public List<Movie> MovieList { get; set; }
+    }
+}
