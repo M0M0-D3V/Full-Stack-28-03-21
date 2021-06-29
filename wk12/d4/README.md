51ed1c3be692181227dda3d4288bc0d8dfc1d65f
Monica Hong <momo.star@gmail.com>
2021-06-29 09:13:08 -0700

    d4 notes

---
+++ wk12/d4/README.md
@@ -0,0 +1,99 @@
+# :tada:WELCOME TO WEEK 12 DAY 4! :tada:
+
+## Today's Agenda
+
+---
+
+## Nominate this week's champion in MatterMost
+
+- Write a message in your team channel who you nominate and reason
+
+### - Will announce tomorrow morning!
+
+---
+
+## :video_game:ACTIVITY DAY:space_invader: - 1pm
+
+### :balloon:Join us here in main room for Activity games! We can take a vote what games we want to play. :)
+
+---
+
+## :page_with_curl:CODE REVIEW - 2:30pm || 3:00pm
+
+### Dojodachi OR
+
+#### - _1. demonstrate functionality_
+
+#### - _2. explain your thought process and workflow_
+
+#### - _3. if you had problems, how did you overcome?_
+
+#### - _4. feedback on code_
+
+- Team 1 - Coding Pirates - 3:00pm with Monica :see_no_evil:
+- Team 2 - Spoiled Coders - 2:30pm with Monica :see_no_evil:
+- Team 6 - Future Developers - 3:00pm with Nathan :guardsman:
+- Team 7 - CodingNinjas - 2:30pm with Alreem :dancer:
+- Team 8 - The Warriors - 3:00pm with Alreem :dancer:
+
+---
+
+## Dojodachi @ 4PM
+
+---
+
+## :school_satchel: Login/Register - ~10am
+
+### - Registration Concerns
+
+- Confirming Password...
+
+```csharp
+// Will not be mapped to your users table!
+    [NotMapped]
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string Confirm {get;set;}
+```
+
+- Validating Unique Email
+
+```csharp
+[HttpPost("register")]
+public IActionResult Register(User user)
+{
+    // Check initial ModelState
+    if(ModelState.IsValid)
+    {
+        // If a User exists with provided email
+        if(dbContext.Users.Any(u => u.Email == user.Email))
+        {
+            // Manually add a ModelState error to the Email field, with provided
+            // error message
+            ModelState.AddModelError("Email", "Email already in use!");
+            // You may consider returning to the View at this point
+        }
+    }
+    // other code
+}
+```
+
+### - Password Hashing
+
+---
+
+#### `gentle reminders...`
+
+- 20 minute rule! - set a timer if you have to
+- ask questions if you're not sure, chances are, your question will help another classmate
+- schedule a :one: on :one: with me on calendly before the exam, let's get you prepared! :muscle:
+
+---
+
+### END OF DAY GOAL: :sparkler: Login Register :sparkler:
+
+---
+
+```
+
+```
---
+++ wk12/d4/algo.js
@@ -0,0 +1,103 @@
+/* **********FOR ATTENDANCE******************
+Work on the algorithm and write each name of participants in the array "Devs" at the very bottom of page.
+Post the code (with the array) to your team chat in MatterMost. It is okay if the solution isn't complete but show you are working on it.
+
+************ WEHN FINISHED ****************
+Read materials for class or continue working on other Project or Assignment after algo is completed and posted.
+*/
+
+// **************** TODAY ********************
+// Starter class Queue
+// [] Queue - implement isSumOfHalvesEqual()
+// [] TwoStackQueue - implement enqueue()
+// [] TwoStackQueue - implement dequeue()
+
+//starter
+class Queue {
+  constructor(items = []) {
+    this.items = items;
+  }
+
+  enqueue(item) {
+    this.items.push(item);
+    return this.size();
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  front() {
+    return this.items[0];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    const str = this.items.join(" ");
+    console.log(str);
+    return str;
+  }
+
+  /**
+   * Determines whether the sum of the left half of the queue items is equal to
+   * the sum of the right half. Avoid indexing the queue items directly via
+   * bracket notation, use the queue methods instead for practice.
+   * Use no extra array or objects.
+   * The queue should be returned to it's original order when done.
+   * - Time: O(n^2) quadratic, n = queue length. Quadratic due to dequeue on an
+   *     array queue being O(n).
+   * - Space: O(1) constant.
+   * @returns {boolean} Whether the sum of the left and right halves is equal.
+   */
+  isSumOfHalvesEqual() {
+    // code here
+  }
+}
+
+/**
+ * Class to represent a Queue but is implemented using two stacks to store the
+ * queued items without using any other objects or arrays to store the items.
+ * Retains the FIFO (First in First Out) ordering when adding / removing items.
+ */
+class TwoStackQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  /**
+   * Adds a new item to the back of the queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @param {any} item To be added.
+   * @returns {number} The new number of items in the queue.
+   */
+  enqueue(item) {
+    // code here
+  }
+
+  /**
+   * Removes the next item in the line / queue.
+   * - Time: O(2n) -> O(n) linear.
+   * - Space: O(1), no extra objects / arrays created within this method itself.
+   * @returns {any} The removed item.
+   */
+  dequeue() {
+    // code here
+  }
+}
+
+/*********** REMEMBER *******************
+1. Think of algorithms like nutrition or going to the gym. A little bit everyday helps you in the longrun.
+2. Imagine if after bootcamp, you want to get ready for your interviews and want to keep practicing algorithms. It would be harder to maintain a habit if you don't already have it.
+3. Benefits of daily algorithms it helps you stay sharp and able to solve problems quicker and more creatively. You will get familiar with more complex strategies and come up with more than 1 way to solve a problem.
+4. Benefit of sharing your solution at the end is practice speaking about code and thought process. 
+*/
+// const Devs = []
